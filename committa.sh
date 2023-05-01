cc5492f8f3e82139a269db6be39b35f6e6c042c5
Stefano Cerruti <stefano.cerruti@outlook.com>
2023-05-01 11:51:37 +0200

    init del repository

--- committa.sh
+++ committa.sh
@@ -1,1 +1,10 @@
-git pull && git add . && git commit -m "Aggiornamento al `date`" && git push && git status
+#!/bin/bash
+
+if [ -z "$1" ]
+  then
+    msg="Aggiornamento al `date -u '+%Y-%m-%d %H:%M:%S UTC'`"
+  else
+    msg="$1"
+fi
+
+git pull && git add . && git commit -m "$msg" && git push && git status
