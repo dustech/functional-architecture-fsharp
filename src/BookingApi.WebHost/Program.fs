2a3b80a2de4557c930751162d685d17fb8946449
Stefano Cerruti <stefano.cerruti@outlook.com>
2024-01-28 16:06:58 +0100

    Move agent implementation to webhost
    Decouple the concurrentqueu implementation from domain Infrastructure.
    
    Now it depends on IReservations interface

--- src/BookingApi.WebHost/Program.fs
+++ src/BookingApi.WebHost/Program.fs
@@ -1,35 +1,65 @@
 namespace Dustech.BookingApi.WebHost
+
 #nowarn "20"
-open System
+
+open System // for Console
 open System.Collections.Concurrent // for ConcurrentBag
 open Dustech.BookingApi.Messages // for Envelop, Reservation
 open Microsoft.AspNetCore.Builder // for WebApplication
 open Dustech.BookingApi.Infrastructure // for ConfigureBuilder
+open Dustech.BookingApi.DomainModel.Reservations // for ToReservations and Handle
+
 module Program =
     let exitCode = 0
-    
-    type HttpRouteDefaults = {Controller:string; Id :obj}
-    
+
+    type HttpRouteDefaults = { Controller: string; Id: obj }
+
+    type Agent<'T> = MailboxProcessor<'T>
+    let seatingCapacity = 10
     
     [<EntryPoint>]
     let main args =
 
         let builder = WebApplication.CreateBuilder(args)
-        
+
         //builder.Services.AddControllers() |> ignore
         //builder.Services.AddSingleton<IControllerActivator>(fun _ -> new BookingApiControllerActivator() :> IControllerActivator) |> ignore
         //builder.Services.AddSingleton<IControllerActivator,BookingApiControllerActivator>() |> ignore
         let db = ConcurrentBag<Envelope<Reservation>>()
-        ConfigureBuilder builder db
-        
+        let agent =
+            new Agent<Envelope<MakeReservation>>(fun inbox ->
+                let rec loop () =
+                    async {
+                        let! cmd = inbox.Receive()
+                        let rs = db |> ToReservations
+                        let handle = Handle seatingCapacity rs
+                        let newReservations = handle cmd
+
+                        match newReservations with
+                        | Some (r) -> db.Add r
+                        | None -> ()
+
+                        Seq.toList rs |> PrintAll
+
+                        return! loop ()
+                    }
+
+                loop ())
+
+        do agent.Start()
+        ConfigureBuilder
+            builder
+            (db |> ToReservations)
+            agent.Post
+
         let app = builder.Build()
-        
+
         Console.WriteLine($"{Environment.NewLine} {app.Environment.EnvironmentName} Environment {Environment.NewLine}")
-                
-                
+
+
         Configure app
-        
-        
+
+
         app.Run()
 
         exitCode
--- src/BookingApi/Infrastructure.fs
+++ src/BookingApi/Infrastructure.fs
@@ -1,57 +1,35 @@
 ﻿module Dustech.BookingApi.Infrastructure
 
 open System // for Type
-open System.Collections.Concurrent // for ConcurrentBag
 open System.Threading.Tasks //  for ValueTask
 open Dustech.BookingApi.Controllers // for custom controllers
-open Dustech.BookingApi.Messages // for Envelope
-open Dustech.BookingApi.DomainModel.Reservations // for ToReservations and Handle
+open Dustech.BookingApi.DomainModel.Reservations // for IReservations
 open Microsoft.AspNetCore.Builder // for WebApplication
 open Microsoft.AspNetCore.Mvc // for ControllerContext
 open Microsoft.AspNetCore.Mvc.Controllers // for IControllerActivator
 open Microsoft.AspNetCore.Routing.Constraints // for OptionalRouteConstraint
 open Microsoft.Extensions.DependencyInjection // for AddSingleton
 
-type Agent<'T> = MailboxProcessor<'T>
+
 
 type HttpRouteDefaults = { Controller: string; Id: obj }
 
-type CompositionRoot(reservations :ConcurrentBag<Envelope<Reservation>>) =
-
-    let seatingCapacity = 10
-
-    let agent =
-        new Agent<Envelope<MakeReservation>>(fun inbox ->
-            let rec loop () =
-                async {
-                    let! cmd = inbox.Receive()
-                    let rs = reservations |> ToReservations
-                    let handle = Handle seatingCapacity rs
-                    let newReservations = handle cmd
-
-                    match newReservations with
-                    | Some(r) -> reservations.Add r
-                    | None -> ()
-                    
-                    Seq.toList rs 
-                    |>  PrintAll
-                    
-                    return! loop ()
-                }
-
-            loop ())
-
-    do agent.Start()
+type CompositionRoot(reservations:IReservations ,reservationRequestObserver) =
 
     let Make (context: ControllerContext, controllerType: Type) : obj =
         match controllerType.Name with
         | nameof HomeController -> HomeController()
         | nameof ReservationController ->
             let c = new ReservationController()
-            let sub = c.Subscribe agent.Post
-            context.HttpContext.Response.RegisterForDispose sub
+
             c
-        | _ -> raise <| InvalidOperationException($"Unknown controller {controllerType}")
+            |> Observable.subscribe reservationRequestObserver
+            |> context.HttpContext.Response.RegisterForDispose
+
+            c
+        | _ ->
+            raise
+            <| InvalidOperationException($"Unknown controller {controllerType}")
 
 
     interface IControllerActivator with
@@ -64,15 +42,19 @@ type CompositionRoot(reservations :ConcurrentBag<Envelope<Reservation>>) =
             | _ -> ()
 
         member me.ReleaseAsync(context, controller) =
-            (me :> IControllerActivator).Release(context, controller) |> ValueTask
+            (me :> IControllerActivator)
+                .Release(context, controller)
+            |> ValueTask
 
 
-let ConfigureServices (builder: WebApplicationBuilder) reservations =
+let ConfigureServices (builder: WebApplicationBuilder) reservations reservationRequestObserver =
     builder.Services.AddControllers() |> ignore
     // builder.Services.AddSingleton<IControllerActivator, CompositionRoot>() |> ignore
-    builder.Services.AddSingleton<IControllerActivator>(fun _ -> new CompositionRoot(reservations) :> IControllerActivator) |> ignore
-        
-        
+    builder.Services.AddSingleton<IControllerActivator> (fun _ ->
+        new CompositionRoot(reservations, reservationRequestObserver) :> IControllerActivator)
+    |> ignore
+
+
 let ConfigureBuilder = ConfigureServices
 
 let ConfigureRoutes (app: WebApplication) =
